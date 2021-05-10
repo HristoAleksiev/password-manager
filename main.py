1fc6f7fd89666b984e26c5cc4a113875f946d1a9
Алексиев <kadoll555@gmail.com>
2021-05-10 23:36:57 +0300

    added method to ingest filled form

--- main.py
+++ main.py
@@ -5,11 +5,22 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
+def ingest_form_data():
+    with open("passwords-database.txt", "a") as pass_db:
+        pass_db.write(f"{website_entry.get()}  |  ")
+        website_entry.delete(0, END)
+        pass_db.write(f"{user_email_entry.get()}  |  ")
+        user_email_entry.delete(0, END)
+        pass_db.write(f"{password_entry.get()}\n")
+        password_entry.delete(0, END)
+        website_entry.focus()
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 
 window = Tk()
-window.tile("Password Manager")
+window.title("Password Manager")
 window.config(padx=20, pady=20)
 
 # Column 1
@@ -31,6 +42,7 @@ canvas.create_image(100, 100, image=logo)
 canvas.grid(column=1, row=0)
 
 website_entry = Entry(width=35)
+website_entry.focus()
 website_entry.grid(column=1, row=1, columnspan=2, sticky="ew")
 
 user_email_entry = Entry(width=35)
@@ -39,7 +51,7 @@ user_email_entry.grid(column=1, row=2, columnspan=2, sticky="ew")
 password_entry = Entry(width=21)
 password_entry.grid(column=1, row=3, sticky="ew", padx=1)
 
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=ingest_form_data)
 add_button.grid(column=1, row=4, columnspan=2, sticky="ew")
 
 # Column 3
