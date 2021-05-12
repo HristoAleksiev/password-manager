0ca4c66acbdbb5db67a37416ad7dec45844cce46
Алексиев <kadoll555@gmail.com>
2021-05-12 22:56:15 +0300

    small refactor: variable rename in comprehension

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 def generate_password():
     password_entry.delete(0, END)
 
-    password_letters = [random.choice(letters) for char in range(random.randint(8, 10))]
-    password_symbols = [random.choice(symbols) for sym in range(random.randint(2, 4))]
-    password_numbers = [random.choice(numbers) for num in range(random.randint(2, 4))]
+    password_letters = [random.choice(letters) for _ in range(random.randint(8, 10))]
+    password_symbols = [random.choice(symbols) for _ in range(random.randint(2, 4))]
+    password_numbers = [random.choice(numbers) for _ in range(random.randint(2, 4))]
 
     password_list = password_letters + password_symbols + password_numbers
     random.shuffle(password_list)
